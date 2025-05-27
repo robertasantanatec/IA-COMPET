91a935be3181ca392cecd0106ff1b3b86d560209
Roberta Santana <robertaxavier24@gmail.com>
2025-05-27 14:51:57 -0300

    Delete IA_Predicao_Projeto/IA_Predicao_Projeto directory

--- IA_Predicao_Projeto/IA_Predicao_Projeto/src/avaliacao.py
+++
@@ -1,6 +0,0 @@
-from sklearn.metrics import accuracy_score, classification_report
-
-def avaliar_modelo(modelo, X_test, y_test):
-    y_pred = modelo.predict(X_test)
-    print("Acurácia:", accuracy_score(y_test, y_pred))
-    print("Relatório:\n", classification_report(y_test, y_pred))
